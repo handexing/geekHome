d0293170b5f08fd4ec1d1de0edb0437f7ec97e41
handexing <908716835@qq.com>
2017-09-12 22:51:41 +0800

    update config

---
+++ geekHome-web-ui/src/main/java/com/geekhome/controller/IndexController.java
@@ -0,0 +1,20 @@
+package com.geekhome.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @Description: 首页
+ * @author handx  
+ * @date 2017年9月12日 下午4:12:30 
+ * @version V1.0
+ */
+@Controller
+@RequestMapping("index")
+public class IndexController {
+
+Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+}
---
+++ geekHome-web-ui/src/main/java/com/geekhome/controller/UserController.java
@@ -0,0 +1,20 @@
+package com.geekhome.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @Description: 用户
+ * @author handx  
+ * @date 2017年9月12日 下午4:07:46 
+ * @version V1.0
+ */
+@Controller
+@RequestMapping("user")
+public class UserController {
+	
+	Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+}
