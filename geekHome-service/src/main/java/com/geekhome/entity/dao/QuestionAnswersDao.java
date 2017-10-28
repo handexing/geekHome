96294fb5ddd5eaa14756a2a72a1ad051e6465ad7
handexing <908716835@qq.com>
2017-10-28 13:53:52 +0800

    update question answers

--- geekHome-service/src/main/java/com/geekhome/entity/dao/QuestionAnswersDao.java
+++ geekHome-service/src/main/java/com/geekhome/entity/dao/QuestionAnswersDao.java
@@ -11,7 +11,8 @@ public interface QuestionAnswersDao extends JpaRepository<QuestionAnswers, Long>
 	@Query(nativeQuery = true, value = "SELECT COUNT(1) FROM QUESTION_ANSWERS WHERE LABEL_ID IN(select ID from label WHERE PARENT_ID=:labelId UNION SELECT :labelId)")
 	int getQuestionAnswersByLabelIdCnt(@Param("labelId") Long labelId);
 
-	@Query(nativeQuery = true, value = "SELECT COUNT(1) FROM QUESTION_ANSWERS WHERE DATE_FORMAT(CREATE_TIME,'%Y-%m-%d') = DATE_FORMAT(NOW(),'%Y-%m-%d')")
+	//WHERE DATE_FORMAT(CREATE_TIME,'%Y-%m-%d') = DATE_FORMAT(NOW(),'%Y-%m-%d')
+	@Query(nativeQuery = true, value = "SELECT COUNT(1) FROM QUESTION_ANSWERS")
 	int getQuestionAnswersByToDayCnt();
 
 }
