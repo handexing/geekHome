4c6b1b0c2c4389bf3132ee4bbd30653de5b6a9b3
yannisPeng <p_y2020@163.com>
2017-11-08 17:30:11 +0800

    add forgetPwd func

--- geekHome-web-ui/src/main/java/com/geekhome/controller/UserController.java
+++ geekHome-web-ui/src/main/java/com/geekhome/controller/UserController.java
@@ -2,10 +2,10 @@ package com.geekhome.controller;
 
 import java.io.PrintWriter;
 import java.util.HashMap;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,13 +13,14 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import com.geekhome.common.utils.EmailUtils;
 import com.geekhome.common.utils.GeetestConfig;
 import com.geekhome.common.utils.GeetestLib;
 import com.geekhome.common.utils.PasswordUtil;
 import com.geekhome.common.vo.ErrorCode;
 import com.geekhome.common.vo.ExecuteResult;
 import com.geekhome.entity.User;
+import com.geekhome.entity.VerifyMessage;
 import com.geekhome.entity.dao.UserDao;
 import com.geekhome.entity.service.UserService;
 
@@ -182,5 +183,74 @@ public class UserController {
 			result.setErrorMsg(ErrorCode.EXCEPTION.getErrorMsg());
 		}
 		return result;
+	}
+	
+	/**
+	 * 发送验证邮件
+	 * @param user
+	 * @return
+	 */
+	@RequestMapping("getEmailCode")
+    @CrossOrigin
+    public ExecuteResult<User> getEmailCode(@RequestBody User user , HttpServletRequest request) {
+        final ExecuteResult<User> result = new ExecuteResult<>();
+        try {
+            User u = userService.verifyByNameAndEmail(user);
+            if(u.getId()!=null){
+                String code = EmailUtils.sendEmail(u.getEmail()); //发送邮件验证
+                result.setData(userService.verifyByNameAndEmail(user));
+                result.setSuccess(true);
+                //将验证码放于session中保存，存放之前先清除
+                if(request.getSession().getAttribute("verifyCode") != null)
+                {
+                    request.getSession().removeAttribute("verifyCode"); 
+                }
+                request.getSession().setAttribute("verifyCode", code);
+            }else {
+                result.setData(new User());
+                result.setSuccess(false);
+            }
+        } catch (final Exception e) {
+            logger.error("", e);
+            result.setSuccess(false);
+            result.setErrorCode(ErrorCode.EXCEPTION.getErrorCode());
+            result.setErrorMsg(ErrorCode.EXCEPTION.getErrorMsg());
+        }
+        return result;
+    }
+	
+	/**
+	 * 修改密码
+	 * @return
+	 */
+	@RequestMapping("modifyPersonPwd")
+    @CrossOrigin
+	public ExecuteResult<User> modifyPersonPwd(@RequestBody VerifyMessage verifyMessage, HttpServletRequest request){
+	    final ExecuteResult<User> result = new ExecuteResult<>();
+	    try{
+	        if ( StringUtils.isNotBlank( verifyMessage.getEmailCode() ) && StringUtils.isNotBlank( verifyMessage.getPassword() ) ){
+	            String code = (String)request.getSession().getAttribute("verifyCode"); 
+	            if(verifyMessage.getEmailCode().equals(code)){
+	                String pwd = PasswordUtil.createCustomPwd(verifyMessage.getPassword(), verifyMessage.getUserName() + User.SALT);
+	                Integer num = userService.modifyPersonPwd(verifyMessage.getUserName(),pwd);
+	                if(num <= 0){
+	                    result.setSuccess(false); 
+	                }
+	                else{
+	                    result.setSuccess(true); 
+	                }
+	            }else {
+	                result.setSuccess(false);
+	                result.setErrorCode(ErrorCode.VERIFY_CODE_WRONG.getErrorCode());
+	                result.setErrorMsg(ErrorCode.VERIFY_CODE_WRONG.getErrorMsg());
+	            }
+	        }
+	    }catch (final Exception e) {
+            logger.error("", e);
+            result.setSuccess(false);
+            result.setErrorCode(ErrorCode.EXCEPTION.getErrorCode());
+            result.setErrorMsg(ErrorCode.EXCEPTION.getErrorMsg());
+        }
+	    return result;
 	}
 }
