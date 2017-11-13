9d24928d3c61622383d3e09272b9b87fdd226e1b
handexing <908716835@qq.com>
2017-11-13 14:13:47 +0800

    update blog

--- geekHome-service/src/main/java/com/geekhome/entity/service/AdminService.java
+++ geekHome-service/src/main/java/com/geekhome/entity/service/AdminService.java
@@ -31,14 +31,14 @@ public class AdminService {
 
 	@SuppressWarnings("unchecked")
 	public Page<Admin> getAdminList(Integer page, Integer rows) {
-
-		final String sql = "SELECT a.ID id ,a.USER_NAME userName,a.`PASSWORD` password,a.STATE state,"
-				+ "a.IS_SYSTEM isSystem,a.SALT salt,a.CREATE_TIME createTime,a.UPDATE_TIME updateTime,r.NAME roleName "
-				+ "FROM admin a" + " LEFT JOIN admin_role ar ON a.ID=ar.ADMIN_ID"
-				+ " LEFT JOIN role r ON ar.ROLE_ID=r.ID";
+		StringBuilder sql = new StringBuilder();
+		sql.append("SELECT a.ID id ,a.USER_NAME userName,a.`PASSWORD` password,a.STATE state,").append(
+				"a.IS_SYSTEM isSystem,a.SALT salt,a.CREATE_TIME createTime,a.UPDATE_TIME updateTime,r.NAME roleName")
+				.append(" FROM admin a LEFT JOIN admin_role ar ON a.ID=ar.ADMIN_ID")
+				.append(" LEFT JOIN role r ON ar.ROLE_ID=r.ID");
 		final int firstRecord = PageUtil.calcPage(page) * rows;
-		final List<Admin> list = entityManager.createNativeQuery(sql, "getAdminList").setFirstResult(firstRecord)
-				.setMaxResults(rows).getResultList();
+		final List<Admin> list = entityManager.createNativeQuery(sql.toString(), "getAdminList")
+				.setFirstResult(firstRecord).setMaxResults(rows).getResultList();
 		final int total = adminDao.getAdminListCnt();
 		final Page<Admin> pages = new PageImpl<>(list, null, total);
 		return pages;
--- geekHome-service/src/main/java/com/geekhome/entity/service/BlogService.java
+++ geekHome-service/src/main/java/com/geekhome/entity/service/BlogService.java
@@ -27,12 +27,15 @@ public class BlogService {
 	@SuppressWarnings("unchecked")
 	public Page<Blog> findBlogByUserIdAndLabelIdList(Long userId, Long typeId, Integer page, int rows) {
 		int firstRecord = PageUtil.calcPage(page) * rows;
-		String sql = "SELECT o.ID id,o.TYPE_ID typeId,o.TITLE title,o.SUBTITLE subtitle,o.BANNER_IMG bannerImg,o.`STATUS` status,o.COLLECT_COUNT collectCount,o.BROWSE_COUNT browseCount,o.CREATE_TIME createTime,"
-				+ "o.UPDATE_TIME updateTime,(SELECT COUNT(1) FROM COMMENT WHERE THEME_ID = o.ID AND TYPE=3) as commentCnt"
-				+ " FROM blog AS o LEFT JOIN BLOG_TYPE AS l ON o.TYPE_ID = l.ID"
-				+ " WHERE TYPE_ID=:typeId AND l.USER_ID=:userId ORDER BY o.CREATE_TIME DESC";
-		List<Blog> list = entityManager.createNativeQuery(sql, "getBlogByUserIdList").setParameter("typeId", typeId)
-				.setParameter("userId", userId).setFirstResult(firstRecord).setMaxResults(rows).getResultList();
+		StringBuilder sql = new StringBuilder();
+		sql.append(
+				"SELECT o.ID id,o.TYPE_ID typeId,o.TITLE title,o.SUBTITLE subtitle,o.BANNER_IMG bannerImg,o.`STATUS` status,o.COLLECT_COUNT collectCount,o.BROWSE_COUNT browseCount,o.CREATE_TIME createTime,")
+				.append("o.UPDATE_TIME updateTime,(SELECT COUNT(1) FROM COMMENT WHERE THEME_ID = o.ID AND TYPE=3) as commentCnt")
+				.append(" FROM blog AS o LEFT JOIN BLOG_TYPE AS l ON o.TYPE_ID = l.ID")
+				.append(" WHERE TYPE_ID=:typeId AND l.USER_ID=:userId ORDER BY o.CREATE_TIME DESC");
+		List<Blog> list = entityManager.createNativeQuery(sql.toString(), "getBlogByUserIdList")
+				.setParameter("typeId", typeId).setParameter("userId", userId).setFirstResult(firstRecord)
+				.setMaxResults(rows).getResultList();
 		int total = blogDao.getBlogByUserIdCnt(userId);
 		Pageable pageable = new PageRequest(page, rows);
 		Page<Blog> pages = new PageImpl<>(list, pageable, total);
@@ -52,11 +55,13 @@ public class BlogService {
 
 	@SuppressWarnings("unchecked")
 	public Blog getBlogById(Long id) {
-		final String sql = "SELECT b.ID id,b.TYPE_ID typeId,b.TITLE title,B.SUBTITLE subTitle,b.CONTENT content,b.COLLECT_COUNT collectCount,b.BROWSE_COUNT browseCount,b.BANNER_IMG bannerImg,b.CREATE_TIME createTime,"
-				+ "b.UPDATE_TIME updateTime,t.`NAME` typeName,u.USER_NAME userName,u.HEAD_IMG_URL headImgUrl,u.ID userId"
-				+ " FROM blog AS b LEFT JOIN blog_type AS t ON t.ID = b.TYPE_ID"
-				+ " LEFT JOIN user AS u ON u.ID = t.USER_ID WHERE b.ID=:id";
-		final List<Blog> list = entityManager.createNativeQuery(sql, "getBlogById").setParameter("id", id)
+		StringBuilder sql = new StringBuilder();
+		sql.append(
+				"SELECT b.ID id,b.TYPE_ID typeId,b.TITLE title,B.SUBTITLE subTitle,b.CONTENT content,b.COLLECT_COUNT collectCount,b.BROWSE_COUNT browseCount,b.BANNER_IMG bannerImg,b.CREATE_TIME createTime,")
+				.append("b.UPDATE_TIME updateTime,t.`NAME` typeName,u.USER_NAME userName,u.HEAD_IMG_URL headImgUrl,u.ID userId")
+				.append(" FROM blog AS b LEFT JOIN blog_type AS t ON t.ID = b.TYPE_ID")
+				.append(" LEFT JOIN user AS u ON u.ID = t.USER_ID WHERE b.ID=:id");
+		final List<Blog> list = entityManager.createNativeQuery(sql.toString(), "getBlogById").setParameter("id", id)
 				.getResultList();
 		return list.get(0);
 	}
@@ -64,10 +69,12 @@ public class BlogService {
 	@SuppressWarnings("unchecked")
 	public Page<Blog> getBlogList(Integer page, Integer rows) {
 		int firstRecord = PageUtil.calcPage(page) * rows;
-		String sql = "SELECT o.ID id,l.ID typeId,t.ID systemTypeId,l.`NAME` typeName,l.`NAME` systemTypeName,o.TITLE title,o.SUBTITLE subtitle,o.CONTENT content,o.BANNER_IMG bannerImg,o.`STATUS` status,o.COLLECT_COUNT collectCount,o.BROWSE_COUNT browseCount,o.CREATE_TIME createTime,"
-				+ "o.UPDATE_TIME updateTime,(SELECT COUNT(1) FROM COMMENT WHERE THEME_ID = o.ID AND TYPE=3) as commentCnt,u.USER_NAME userName,u.HEAD_IMG_URL headImgUrl,l.USER_ID userId"
-				+ " FROM blog AS o LEFT JOIN BLOG_TYPE AS l ON o.TYPE_ID = l.ID LEFT JOIN BLOG_TYPE AS t ON o.SYSTEM_TYPE_ID = t.ID LEFT JOIN user AS u ON u.ID = l.USER_ID ORDER BY o.CREATE_TIME DESC";
-		List<Blog> list = entityManager.createNativeQuery(sql, "getAllBlogList").setFirstResult(firstRecord)
+		StringBuilder sql = new StringBuilder();
+		sql.append(
+				"SELECT o.ID id,l.ID typeId,t.ID systemTypeId,l.`NAME` typeName,t.`NAME` systemTypeName,o.TITLE title,o.SUBTITLE subtitle,o.CONTENT content,o.BANNER_IMG bannerImg,o.`STATUS` status,o.COLLECT_COUNT collectCount,o.BROWSE_COUNT browseCount,o.CREATE_TIME createTime,")
+				.append("o.UPDATE_TIME updateTime,(SELECT COUNT(1) FROM COMMENT WHERE THEME_ID = o.ID AND TYPE=3) as commentCnt,u.USER_NAME userName,u.HEAD_IMG_URL headImgUrl,l.USER_ID userId")
+				.append(" FROM blog AS o LEFT JOIN BLOG_TYPE AS l ON o.TYPE_ID = l.ID LEFT JOIN BLOG_TYPE AS t ON o.SYSTEM_TYPE_ID = t.ID LEFT JOIN user AS u ON u.ID = l.USER_ID ORDER BY o.CREATE_TIME DESC");
+		List<Blog> list = entityManager.createNativeQuery(sql.toString(), "getAllBlogList").setFirstResult(firstRecord)
 				.setMaxResults(rows).getResultList();
 		int total = blogDao.getBoleCnt();
 		Pageable pageable = new PageRequest(page, rows);
